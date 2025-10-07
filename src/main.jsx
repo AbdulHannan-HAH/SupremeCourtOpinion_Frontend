48c82cd81e4436627e582283c3459357cc4abfad
AbdulHannan-HAH <2023cs802@student.uet.edu.pk>
2025-10-07 04:54:38 -0700

    WWW

--- src/main.jsx
+++ src/main.jsx
@@ -6,8 +6,10 @@ import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
+    <HashRouter>
     <BrowserRouter>
       <App />
     </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
